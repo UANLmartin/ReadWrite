ac6a733d61142273ab37970af153fb47b843b061
martinnv6 <martin.nv6@gmail.com>
2017-11-29 11:58:11 -0600

    Add nuew information and ico

--- SelectResource.cs
+++ SelectResource.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Drawing;
-using System.Collections;
-using System.ComponentModel;
 using System.Windows.Forms;
 using NationalInstruments.VisaNS;
+
 
 namespace NationalInstruments.Examples.SimpleReadWrite
 {
